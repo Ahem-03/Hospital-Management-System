437655cfaec6910117d954cbfa5530b54803daae
Ahem-03 <ahemsharma9026@gmail.com>
2025-12-06 13:28:16 +0530

    searching button by ID or NAME

--- D_dashboard.java
+++ D_dashboard.java
@@ -7,11 +7,11 @@ import java.sql.*;
 public class D_dashboard extends JFrame {
     JPanel p, pApmtJPanel;
     JTextArea tArea;
-    JLabel lblapmt, jdtitle, jdoctorInfo, lblPatientID, lblPatientName, lblSymptoms, lblDiagnosis, lblMedicines,
+    JLabel lblapmt, jdtitle, jdoctorInfo, lblAppointmentID, lblPatientName, lblSymptoms, lblDiagnosis, lblMedicines,
             lblAdvice,
             lblNextVisit;
-    JTextField txtPatientID, txtPatientName, txtSymptoms, txtDiagnosis, txtMedicines, txtAdvice, txtNextVisit;
-    JButton btnSave, btnView, btnLogout, btnSearch;
+    JTextField txtAppointmentID, txtPatientName, txtSymptoms, txtDiagnosis, txtMedicines, txtAdvice, txtNextVisit;
+    JButton btnSave, btnView, btnLogout, btnSearch, btnRefresh;
 
     // Table for appointments
     DefaultTableModel model;
@@ -50,7 +50,7 @@ public class D_dashboard extends JFrame {
         jdoctorInfo.setForeground(Color.DARK_GRAY);
 
         // Labels
-        lblPatientID = new JLabel("Appointment ID:");
+        lblAppointmentID = new JLabel("Appointment ID:                              Name");
         lblPatientName = new JLabel("OR");
         lblSymptoms = new JLabel("Symptoms:");
         lblDiagnosis = new JLabel("Diagnosis:");
@@ -58,7 +58,7 @@ public class D_dashboard extends JFrame {
         lblAdvice = new JLabel("Advice:");
         lblNextVisit = new JLabel("Next Visit Date:");
         // patient label font setting
-        lblPatientID.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+        lblAppointmentID.setFont(new Font("Segoe UI", Font.PLAIN, 15));
         lblPatientName.setFont(new Font("Segoe UI", Font.PLAIN, 15));
         lblSymptoms.setFont(new Font("Segoe UI", Font.PLAIN, 15));
         lblDiagnosis.setFont(new Font("Segoe UI", Font.PLAIN, 15));
@@ -67,7 +67,7 @@ public class D_dashboard extends JFrame {
         lblNextVisit.setFont(new Font("Segoe UI", Font.PLAIN, 15));
 
         // Text fields
-        txtPatientID = new JTextField();
+        txtAppointmentID = new JTextField();
         txtPatientName = new JTextField();
         txtSymptoms = new JTextField();
         txtDiagnosis = new JTextField();
@@ -79,23 +79,26 @@ public class D_dashboard extends JFrame {
         btnSearch = new JButton("🔍");
         btnSave = new JButton("💾  Save Prescription");
         btnView = new JButton("📋  View Appointments");
+        btnRefresh = new JButton("🔄 Refresh");
         btnLogout = new JButton("🚪 Logout");
         // backgrund color
         btnSearch.setBackground(new Color(0, 102, 204));
         btnSave.setBackground(new Color(0, 153, 76));
         btnView.setBackground(new Color(0, 102, 204));
+        btnRefresh.setBackground(new Color(0, 102, 204));
         btnLogout.setBackground(new Color(204, 0, 0));
         // color
         btnSearch.setForeground(Color.WHITE);
         btnSave.setForeground(Color.WHITE);
         btnView.setForeground(Color.WHITE);
+        btnRefresh.setForeground(Color.WHITE);
         btnLogout.setForeground(Color.WHITE);
 
         // Add components to panel
         // label
         p.add(jdtitle);
         p.add(jdoctorInfo);
-        p.add(lblPatientID);
+        p.add(lblAppointmentID);
         p.add(lblPatientName);
         p.add(lblSymptoms);
         p.add(lblDiagnosis);
@@ -103,7 +106,7 @@ public class D_dashboard extends JFrame {
         p.add(lblAdvice);
         p.add(lblNextVisit);
         // text field
-        p.add(txtPatientID);
+        p.add(txtAppointmentID);
         p.add(txtPatientName);
         p.add(txtSymptoms);
         p.add(txtDiagnosis);
@@ -119,6 +122,7 @@ public class D_dashboard extends JFrame {
         add(p);
         p.add(pApmtJPanel);
         pApmtJPanel.add(lblapmt);
+        pApmtJPanel.add(btnRefresh);
 
         // Create table for appointments
         model = new DefaultTableModel();
@@ -146,11 +150,11 @@ public class D_dashboard extends JFrame {
         jdoctorInfo.setBounds(0, 70, 1050, 30);
 
         // Left side for patient details
-        lblPatientID.setBounds(20, 140, 120, 30);
-        txtPatientID.setBounds(140, 140, 80, 30);
+        lblAppointmentID.setBounds(20, 100, 500, 30);
+        txtAppointmentID.setBounds(110, 140, 80, 30);
 
-        lblPatientName.setBounds(225, 140, 30, 30);
-        txtPatientName.setBounds(260, 140, 80, 30);
+        lblPatientName.setBounds(200, 140, 30, 30);
+        txtPatientName.setBounds(230, 140, 80, 30);
 
         lblSymptoms.setBounds(20, 190, 120, 30);
         txtSymptoms.setBounds(140, 190, 200, 30);
@@ -168,9 +172,10 @@ public class D_dashboard extends JFrame {
         txtNextVisit.setBounds(140, 390, 200, 30);
 
         // Buttons below form
-        btnSearch.setBounds(350, 140, 80, 30);
+        btnSearch.setBounds(320, 140, 50, 30);
         btnSave.setBounds(20, 460, 150, 35);
         btnView.setBounds(180, 460, 160, 35);
+        btnRefresh.setBounds(550, 10, 100, 30);
         btnLogout.setBounds(30, 720, 100, 35);
 
         // Action Listerner
@@ -190,6 +195,11 @@ public class D_dashboard extends JFrame {
                 loadAppointments();
                 pApmtJPanel.setVisible(true);
             }
+        });
+         btnRefresh.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                loadAppointments();
+            } 
         });
         btnLogout.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -234,22 +244,53 @@ public class D_dashboard extends JFrame {
 
     //// ===========stuck in to if condition
     public void searching() {
-        String patient_ID = "%" + txtPatientID.getText().trim() + "%";
-        if (patient_ID.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Please fill the ID or NAME ", patient_ID, JOptionPane.WARNING_MESSAGE);
+        String appointment_id = txtAppointmentID.getText().trim();
+        String patient_name = txtPatientName.getText().trim();
+        if (appointment_id.isEmpty() && patient_name.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please fill the ID or NAME ", appointment_id,
+                    JOptionPane.WARNING_MESSAGE);
             return;
         }
         try {
             Connection con = DriverManager.getConnection(url, user, user_password);
-            stmt = con.createStatement();
-            String query_1 = "SELECT patient_name FROM appointments where appointment_id like ?";
-            System.out.println("first qury is running ");
-            PreparedStatement pst = con.prepareStatement(query_1);
-            pst.setString(1, patient_ID);
 
-            ResultSet rs = pst.executeQuery();
-            if (rs.next()) {
-                txtPatientName.setText(rs.getString("patient_name"));
+            // =========== Fiil the Patient name area when you enter the appointment_ID
+            // ===========
+            if (!appointment_id.isEmpty() && patient_name.isEmpty()) {
+                String query_1 = "SELECT patient_name FROM appointments where appointment_id like ? ";
+                System.out.println("first qury is running ");
+                try {
+                    PreparedStatement pst = con.prepareStatement(query_1);
+                    pst.setString(1, "%"+appointment_id+"%");
+                    ResultSet rs = pst.executeQuery();
+                    if (rs.next()) {
+                        txtPatientName.setText(rs.getString("patient_name"));
+                    }
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(this, "Error loading " + e.getMessage(), "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                    e.printStackTrace();
+                }
+            }
+
+            // =========== Fiil the appointment id area when you enter the name ===========
+            else if (!patient_name.isEmpty() && appointment_id.isEmpty()) {
+                String query_2 = "SELECT appointment_id FROM appointments where patient_name like ?";
+                System.out.println("second qury is running ");
+                try {
+                    PreparedStatement pst = con.prepareStatement(query_2);
+                    pst.setString(1, "%" + patient_name + "%");
+                    ResultSet rs = pst.executeQuery();
+                    if (rs.next()) {
+                        txtAppointmentID.setText(rs.getString("appointment_id"));
+                    } else {
+
+                    }
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(this, "Error loading " + e.getMessage(), "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                    e.printStackTrace();
+                }
             }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(this, "Error loading appointments: " + e.getMessage(), "Error",
@@ -258,24 +299,30 @@ public class D_dashboard extends JFrame {
         }
     }
 
-/// ================also stuck in insrting the data into the table =======
+    /// ================also stuck in insrting the data into the table =======
     public void savePrescription() {
+        String appointment_ID = txtAppointmentID.getText().trim();
+        String patientName = txtPatientName.getText().trim();
+        String Symptoms = txtSymptoms.getText().trim();
+        String diagnosis = txtDiagnosis.getText().trim();
+        String medicine = txtMedicines.getText().trim();
+        String advice = txtAdvice.getText().trim();
+        String date = txtNextVisit.getText().trim();
+
+        //validation
+        if (Symptoms.isEmpty() || diagnosis.isEmpty() || medicine.isEmpty() || advice.isEmpty() || date.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please fill all filds", "validation",JOptionPane.WARNING_MESSAGE);
+        return;
+        }
         try {
-            con =DriverManager.getConnection(url, user, user_password);
+            con = DriverManager.getConnection(url, user, user_password);
+            stmt = con.createStatement();
 
-            String patient_ID = txtPatientID.getText().trim();
-            String patientName = txtPatientName.getText().trim();
-            String Symptoms = txtSymptoms.getText().trim();
-            String diagnosis = txtDiagnosis.getText().trim();
-            String medicine = txtMedicines.getText().trim();
-            String advice = txtAdvice.getText().trim();
-            String date = txtNextVisit.getText().trim();
-
-            String qury_2 = "insert into patient_prec(patient_id, patient_name, Symptoms, Diagnosis, Medicines, Advice, next_visit_date) values(?, ?, ?, ?, ?, ?, ?)";
+            String qury_2 = "insert into patient_prec(appointment_id, patient_name, Symptoms, Diagnosis, Medicines, Advice, next_visit_date) values(?, ?, ?, ?, ?, ?, ?)";
 
             System.out.println("second qury is running..... ");
             PreparedStatement pst_2 = con.prepareStatement(qury_2);
-            pst_2.setString(1, patient_ID);
+            pst_2.setString(1, appointment_ID);
             pst_2.setString(2, patientName);
             pst_2.setString(3, Symptoms);
             pst_2.setString(4, diagnosis);
@@ -285,6 +332,7 @@ public class D_dashboard extends JFrame {
 
             pst_2.executeUpdate();
             JOptionPane.showMessageDialog(this, "Record Inseted");
+            clearForm();
         } catch (Exception e) {
             JOptionPane.showMessageDialog(this, "Error loading appointments: " + e.getMessage(), "Error",
                     JOptionPane.ERROR_MESSAGE);
@@ -292,6 +340,15 @@ public class D_dashboard extends JFrame {
         }
     }
 
+    public void clearForm(){
+        txtAppointmentID.setText("");
+                txtPatientName.setText("");
+                txtSymptoms.setText("");
+                txtDiagnosis.setText("");
+                txtMedicines.setText("");
+                txtAdvice.setText("");
+                txtNextVisit.setText("");
+    }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new D_dashboard());
     }
